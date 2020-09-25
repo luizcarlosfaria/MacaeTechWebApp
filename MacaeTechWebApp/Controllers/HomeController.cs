ce1619931c18c578b61f7c3c8ddd9475287df969
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2020-09-25 19:44:39 -0300

    v1

--- MacaeTechWebApp/Controllers/HomeController.cs
+++ MacaeTechWebApp/Controllers/HomeController.cs
@@ -6,20 +6,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MacaeTechWebApp.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace MacaeTechWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            this.ViewBag.ConnectionString = configuration.GetValue<string>("MinhaApp:Connectionstring");
             return View();
         }
 
